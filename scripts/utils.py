31d8f37fda62b37091e3f751e3961d67f1b3c823
Ronald Torres <ronald26d18@gmail.com>
2021-03-10 14:36:55 -0500

    Add slugify last option and fix buttons descriptions

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,33 +14,34 @@ SHADING = ['mask', 'polyRender', 'occlusion', 'defaultShader', 'advancedMaterial
 ITEMS = ['mesh', 'groupLocator', 'camera', 'locator', '*Light', 'portal', 'falloff.*', '*.falloff', 'curveParam']
 GROUPS = [1, 'render', 'actionclip']
 
-selected = scene.selected
+selection = scene.selected
 
 def renameObject(obj, new_name):
     obj.select(True)
     lx.eval('item.name "{0}"'.format(new_name)) # Have to rename with lx.eval otherwise undo doesn't work
 
 def isInGroup(itemType, target):
-
     for t in target:
         if (t == itemType) or (isinstance(itemType, str) and isinstance(t, str) and (t.find('*') != -1) and (itemType.find(t.replace('*', '')) != -1)):
             return True
 
     return False
-        
 
-target = None
+def renameByTargetType(target):
+    for item in selection:
+        itemType = item.type or item.Type()
+        if isInGroup(itemType, target):
+            renameObject(item, slugify(item.baseName or item.name))
+
 
 if arg == 'shading':
-    target = SHADING
+    renameByTargetType(SHADING)
 if arg == 'items':
-    target = ITEMS
+    renameByTargetType(ITEMS)
 if arg == 'groups':
-    target = GROUPS
+    renameByTargetType(GROUPS)
+if arg == 'last':
+    item = selection[-1]
+    renameObject(item, slugify(item.baseName or item.name))
 # if arg == 'all':
-#     target = GROUPS + SHADING + ITEMS
-
-for item in selected:
-    itemType = item.type or item.Type()
-    if isInGroup(itemType, target):
-	    renameObject(item, slugify(item.baseName or item.name))
+#     renameByTargetType(GROUPS + SHADING + ITEMS)
